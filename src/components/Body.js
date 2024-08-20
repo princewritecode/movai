5b7024d0cb1cce5025ffeccca1eccf78353d2a6e
princewritecode <princewritecode@gmail.com>
2024-08-20 12:13:21 +0530

    created form sign in sign up version 1

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Login from './Login';
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Browse from './Browse';
 const Body = () =>
 {
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,11 @@ import { Link } from 'react-router-dom'; // Ensure this is correct
 const Header = () =>
 {
     return (
-        <div>
+        <div className='z-10 absolute px-8 py-2 bg-gradient-to-b from-black'>
             {/* <Link to="/">Home</Link>
             <Link to="login">Login</Link>
             <Link to="browse">Browse</Link>  */}
-
-            <img src=''></img>
+            <img className='w-44 ' src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo'></img>
         </div>
     );
 };
